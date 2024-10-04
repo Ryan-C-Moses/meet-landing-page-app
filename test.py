fe278866533955c13155ecef811a5e1b410c713d
Ryan-C-Moses <ryancmoses@outlook.com>
2024-10-04 16:05:47 -0400

    Test 5 for scripting

---
+++ test.py
@@ -0,0 +1,9 @@
+str = '\nThis is just a placeholder for some python code, in the future, strictly for learning purposes I may try to run python scripts as pre-commit hooks and npm test'
+
+notice = 'Please Disregard\n'
+
+def print_notice(): 
+ return str + '\n' + notice
+
+
+print_notice()
